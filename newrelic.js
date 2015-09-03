6f56bb0c5bb2306f9e37343f106e7d4f0c2e80c8
James Fleeting <hello@jamesfleeting.com>
2015-09-03 15:47:42 -0400

    Adding newrelic add-on.

---
+++ newrelic.js
@@ -0,0 +1,7 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = { };
